f47bfabae6f700968b5064db877f041c6ca28dd3
julianedeveloper <dev.juliane@gmail.com>
2022-07-05 18:04:01 -0300

    filter, regEx  e Substring

---
+++ js/filtrar.js
@@ -0,0 +1,26 @@
+var campoFiltro = document.querySelector("#filtrar-tabela");
+
+campoFiltro.addEventListener("input", function(){
+    console.log(this.value);
+    var pacientes = document.querySelectorAll(".paciente");
+
+    if (this.value.length > 0) {
+        console.log('Tem algo digitado.');
+        for(var i = 0; i < pacientes.length; i++) {
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+            var expressao = new RegExp(this.value,'i'); //aqui com o 'i' eu digo que aceito qualquer tamanho de letra
+            if (!expressao.test(nome)) {
+                paciente.classList.add("invisivel");
+            } else {
+                paciente.classList.remove("invisivel");
+            }
+        }
+    } else {
+        for(var i = 0; i < pacientes.length; i++){
+            var paciente = pacientes[i];
+            paciente.classList.remove('invisivel');
+        }
+    }
+});
--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -13,6 +13,13 @@ pacientes.forEach(function(paciente){
 */
 
 var tabela = document.querySelector('table');
+
 tabela.addEventListener('dblclick', function(event){
+event.target.parentNode.classList.add('fadeOut'); //aqui eu adiciono (classList.add) o efeito criado no css (fadeOut)
+
+setTimeout(function() {
     event.target.parentNode.remove();
-})
+}, 1000);
+
+
+})                                                                              
